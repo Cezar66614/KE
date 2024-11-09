1f4183f88222c0cd0e2bfdb3ee63b2d24a3fbfc5
K32ar <cezarrusu20.000@gmail.com>
2024-11-10 01:00:56 +0200

    Done? At least for the start. Implemented a stack that holds scopes and each scope multiple elements Each element is either a pointer or a function Every pointer will get freed and every function will get called if there is an error triggered. Everything cleans up nicely Might macroify the function calls to make the code easier to type

--- src/main.c
+++ src/main.c
@@ -1,7 +1,45 @@
+#define KE_IMPL
+#include "ke.h"
+
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello, World!\n");
+  ke_init();
 
+  ke_push_scope(true);
+
+  char *a = malloc(sizeof(char) * 16);
+  int i; for (i = 0; i <= 9; ++i) a[i] = '0' + i;
+  a[i] = '\0';
+
+  printf("%s\n", a);
+
+  ke_push_elem(a, NULL);
+
+  {
+    ke_push_scope(false);
+
+    char *b = malloc(sizeof(char) * 16);
+    int i; for (i = 0; i <= 9; ++i) b[i] = '9' - i;
+    b[i] = '\0';
+
+    printf("%s\n", b);
+
+    ke_push_elem(b, NULL);
+
+    {
+      ke_push_scope(false);
+
+      ke_error("Test");
+
+      //ke_pop_scope();
+    }
+
+    ke_pop_scope();
+  }
+
+  //ke_pop_scope();
+
+  ke_free();
   return 0;
 }
